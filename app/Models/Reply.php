41422401279572082d9d90e8d05d47af2afcf7d1
Martín Valenzuela Magallanes <martynvalenz@gmail.com>
2020-06-28 11:18:59 -0600

    model relationships

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Models\Question;
+use App\Models\Like;
 
 class Reply extends Model
 {
-    //
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function like(){
+        return $this->hasMany(Like::class);
+    }
 }
